0ed81b27b720f0fe59f1693fdf3b528344b603ca
QuynhDinh <quynhndinh96@gmail.com>
2021-11-15 14:50:59 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,21 @@ chooseBtn.addEventListener ('click', function(event){
     console.log(selectedPokemon);
     window.location.href(selectedPokemon + "Pokedex.html");
 });
+
+// youtube api = 'https://www,googleapis.com/youtube/v3/playListItems';
+const api_key = '&key=AIzaSyCxiSCXwO-sJjxWJhf-__CeSTm5SIcjvDA';
+const playlistID = 'PL7B75E2DC0828B037'
+  
+const youtubeUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='
+
+let youtubeAPI = youtubeUrl +
+  playlistID +
+  api_key;
+
+fetch(youtubeAPI) 
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      })
